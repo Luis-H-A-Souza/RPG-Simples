e85159a6bd659a0f20be68f671f9b05e49df6218
Luis Souza <luisricksouza@gmail.com>
2024-11-11 20:41:05 -0300

    Feat: Adiciona metodos de combate para Personagem

--- RPG/src/rpgPackage/Personagem.java
+++ RPG/src/rpgPackage/Personagem.java
@@ -1,15 +1,26 @@
 package rpgPackage;
 
-public class Personagem {
+public abstract class Personagem {
 	private String nome;
 	private int vida;
 	private int defesa;
 	private int ataque;
-	
-	public Personagem (String nome, int vida, int defesa, int ataque) {
+
+	public Personagem(String nome, int vida, int defesa, int ataque) {
 		this.nome = nome;
 		this.vida = vida;
 		this.ataque = ataque;
 		this.defesa = defesa;
+	}
+
+	public abstract int atacar();
+
+	public void receberDano(int dano) {
+		vida -= Math.max(dano - defesa, 0);
+		System.out.println(nome + " Recebeu " + dano + " de dano!\nVida restante: " + vida);
+	}
+	
+	public boolean estaVivo() {
+		return vida > 0;
 	}
 }
