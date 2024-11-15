0220f3d2f2fec9666a21271f19ef284d8de7fad8
Luis Souza <luisricksouza@gmail.com>
2024-11-14 22:56:18 -0300

    Feat: Adiciona metodos de combate

--- RPG/src/rpgPackage/Combate.java
+++ RPG/src/rpgPackage/Combate.java
@@ -1,5 +1,71 @@
 package rpgPackage;
 
-public class Combate {
+import java.util.Scanner;
 
+public class Combate {
+	Scanner sc = new Scanner(System.in);
+	private Heroi heroi;
+	private Inimigo inimigo;
+
+	public Heroi getHeroi() {
+		return heroi;
+	}
+
+	public void setHeroi(Heroi heroi) {
+		this.heroi = heroi;
+	}
+
+	public Inimigo getInimigo() {
+		return inimigo;
+	}
+
+	public void setInimigo(Inimigo inimigo) {
+		this.inimigo = inimigo;
+	}
+
+	public Combate(Heroi heroi, Inimigo inimigo) {
+		this.heroi = heroi;
+		this.inimigo = inimigo;
+	}
+
+	public void turnoJogador() {
+		int aux = 0;
+		System.out.println("\nEscolha o que deseja fazer:");
+		do {
+			System.out.println("1 - Ataque Básico\n2 - Ataque Especial");
+			System.out.print("\nInforme sua ação: ");
+			aux = sc.nextInt();
+			if (aux != 1 && aux != 2) {
+				System.out.println("\nAção Invalida!\n");
+			}
+		} while (aux != 1 && aux != 2);
+		if (aux == 1) {
+			int dano = heroi.atacar();
+			inimigo.receberDano(dano);
+		} else {
+			int dano = heroi.ataqueEspecial();
+			inimigo.receberDano(dano);
+		}
+	}
+
+	public void turnoInimigo() {
+		int dano = inimigo.atacar();
+		heroi.receberDano(dano);
+	}
+
+	public void iniciarCombate() {
+		System.out.println(heroi.getNome() + " Versus " + inimigo.getNome());
+		do {
+			turnoJogador();
+			if (inimigo.estaVivo()) {
+				turnoInimigo();
+			}
+		} while (heroi.estaVivo() && inimigo.estaVivo());
+
+		if (heroi.estaVivo()) {
+			System.out.println(heroi.getNome() + "Venceu o combate!");
+		} else {
+			System.out.println(inimigo.getNome() + "Venceu o combate!");
+		}
+	}
 }
